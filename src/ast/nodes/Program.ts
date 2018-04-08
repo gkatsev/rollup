1f64e80be9b248276acbb940aebcb32fa0a4084a
Lukas Taegert <lukas.taegert@tngtech.com>
2018-04-08 15:34:32 +0200

    Only start another tree-shaking pass if a variable was included; saves one pass

--- src/ast/nodes/Program.ts
+++ src/ast/nodes/Program.ts
@@ -8,14 +8,14 @@ export default class Program extends NodeBase {
 	body: StatementNode[];
 
 	include() {
-		let addedNewNodes = !this.included;
+		let anotherPassNeeded = false;
 		this.included = true;
 		for (const node of this.body) {
 			if (node.shouldBeIncluded() && node.include()) {
-				addedNewNodes = true;
+				anotherPassNeeded = true;
 			}
 		}
-		return addedNewNodes;
+		return anotherPassNeeded;
 	}
 
 	render(code: MagicString, options: RenderOptions) {
